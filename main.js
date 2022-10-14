735e9a7973db5cc1bbec17198f78d3071c04030b
hoaiphuc99 <cvhphuc99@gmail.com>
2022-10-14 22:19:42 +0700

    baitap

---
+++ main.js
@@ -0,0 +1,61 @@
+// BÀI TẬP 1 : tính tiền lương nhân viên
+// --đầu vào
+var luong = 100000;
+var ngaylam = 5;
+// xử lý
+var tienluong = luong * ngaylam;
+// đầu ra
+console.log("tiền lương là:" + tienluong);
+
+
+
+
+// BÀI TẬP 2 :TÍNH GIÁ TRỊ TRUNG BÌNH
+// đầu vào
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+// xử lý
+var sum = a + b + c + d + e;
+var avg = sum / 5;
+// đầu ra 
+console.log("giá trị trung bình là:" + avg);
+
+
+// BÀI TẬP 3: QUY ĐỔI TIỀN
+// đầu vào
+var usd = 23500;
+var nhap = 1000;
+// xử lý
+var giatri = usd * nhap;
+// đầu ra 
+console.log("giá trị là:" + giatri + "VND")
+
+
+
+// BÀI TẬP 4 : TÍNH DIỆN TÍCH,CHU VI HÌNH CHỮ NHẬT 
+// đầu vào
+var dai = 5;
+var rong = 3;
+// xử lý 
+var dientich = dai * rong;
+var chuvi = (dai + rong) * 2;
+// đầu ra 
+console.log("diện tích hcn là: " + dientich + "cm2");
+console.log("chu vi hcn là: " + chuvi + "cm");
+
+
+
+
+// BÀI TẬP 5: TÍNH TỔNG 2 KÍ SỐ 
+// đầu vào 
+var n = 69;
+// xử lý
+var hangchuc = Math.floor(n / 10);
+var donvi = n % 10;
+var tong = hangchuc + donvi;
+// đầu ra 
+
+console.log("tổng 2 kí số là: " + tong);
